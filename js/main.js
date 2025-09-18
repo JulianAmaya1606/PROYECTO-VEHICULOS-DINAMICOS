12840d3bae960e666b0578a94165498c2b7344f1
Julián Amaya <julianamaya3546@gmail.com>
2025-09-18 17:39:49 -0500

    se empieza a crear las constantes globales

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('form-vehiculo');
+const foto = document.getElementById('foto').value;
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    
+
+
+
+
+
+})
